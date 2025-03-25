e3cdc45e85ec285b73f54a987b5ad679d22cb0fb
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-03-25 14:05:15 +0300

    Initial Commit

---
+++ SocketAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketAPI
+//
+//  Created by Ahmet Hakan Asaroğlu on 25.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SocketAPITests/SocketAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  SocketAPITests.swift
+//  SocketAPITests
+//
+//  Created by Ahmet Hakan Asaroğlu on 25.03.2025.
+//
+
+import Testing
+@testable import SocketAPI
+
+struct SocketAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
